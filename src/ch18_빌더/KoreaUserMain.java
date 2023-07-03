79bdb67370bf74b6750b189e11b1bb6262016e5a
comet7406 <jsp7406@naver.com>
2023-07-03 17:40:24 +0900

    feat: 07.03 오후수업

---
+++ src/ch18_빌더/KoreaUserMain.java
@@ -0,0 +1,18 @@
+package ch18_빌더;
+
+public class KoreaUserMain {
+
+	public static void main(String[] args) {
+		
+		KoreaUser koreaUser = KoreaUser.builder().userId(1).build();
+//		KoreaUser koreaUser = KoreaUser.builder()
+//				.userId(1)
+//				.username("aaa")
+//				.password("1234")
+//				.name("정혜성")
+//				.email("aaa.@gmail.com")
+//				.build();
+//
+	}
+	
+}
